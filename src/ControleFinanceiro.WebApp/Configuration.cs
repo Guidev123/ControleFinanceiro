62e3becb756b15c92165bdc168f6e7f8adab3888
Guilherme <guirafaelrn@gmail.com>
2024-07-04 10:49:27 -0300

    feat: create a dark and light theme

---
+++ src/ControleFinanceiro.WebApp/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using MudBlazor;
+
+namespace ControleFinanceiro.WebApp
+{
+    public static class Configuration
+    {
+        public static MudTheme theme = new()
+        {
+            Typography = new Typography
+            {   
+                Default = new Default
+                {
+                    FontFamily = ["Teko", "sons-serif"]
+                }
+            },
+            PaletteDark = new PaletteDark
+            {
+                Primary = "#040943",
+                Secondary = Colors.LightBlue.Darken3,
+                AppbarBackground = "#040943",
+                AppbarText = "#d5d7d9"
+            },
+            PaletteLight = new PaletteLight
+            {
+                Primary = "#ffffff",
+                Secondary = Colors.LightBlue.Darken3,
+                Background = Colors.Gray.Lighten4,
+                AppbarBackground = "#ffffff",
+                AppbarText = Colors.Shades.Black,
+                TextPrimary = Colors.Shades.Black,
+                PrimaryContrastText = Colors.Shades.White,
+                DrawerText = Colors.Shades.Black,
+                DrawerBackground = Colors.LightBlue.Lighten4
+            }
+        };
+    }
+}
